c1c8c9755fa5c8cb08debc16c0e20f85087d7d48
lptree <lphnl1@163.com>
2018-04-12 06:25:28 +0800

    多数据库配置延后
    生成代码模板也需要改动，待有空时修改

--- src/main/java/com/letgym/gymcourse/configurer/MybatisConfigurer_MultiDataSource.java
+++ src/main/java/com/letgym/gymcourse/configurer/MybatisConfigurer_MultiDataSource.java
@@ -1,9 +1,13 @@
 package com.letgym.gymcourse.configurer;
 
+import org.springframework.context.annotation.Configuration;
+
 /*
 * 多数据库配置。
 * https://www.cnblogs.com/ityouknow/p/6102399.html
+* https://github.com/ityouknow/spring-boot-examples/tree/master/spring-boot-mybatis-mulidatasource
 * */
+//@Configuration
 public class MybatisConfigurer_MultiDataSource {
 
 }
